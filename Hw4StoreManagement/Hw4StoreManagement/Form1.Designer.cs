45bed26066a71b396f5b6b4fd7e98913d04bc7a5
IrisAndMimi <my1ris227@outlook.com>
2018-05-01 16:22:23 +0800

    大致完成?

--- Hw4StoreManagement/Hw4StoreManagement/Form1.Designer.cs
+++ Hw4StoreManagement/Hw4StoreManagement/Form1.Designer.cs
@@ -28,23 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbx_cost = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbx_pay = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // tbx_cost
             // 
-            this.textBox1.Location = new System.Drawing.Point(281, 105);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "12";
+            this.tbx_cost.Location = new System.Drawing.Point(281, 105);
+            this.tbx_cost.Name = "tbx_cost";
+            this.tbx_cost.Size = new System.Drawing.Size(100, 22);
+            this.tbx_cost.TabIndex = 0;
+            this.tbx_cost.Text = "12";
             // 
             // label1
             // 
@@ -73,7 +73,7 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(190, 20);
             this.comboBox1.TabIndex = 3;
-            this.comboBox1.Text = "黑松汽水";
+            this.comboBox1.Text = "--請輸入品名--";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label3
@@ -96,13 +96,13 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "售價";
             // 
-            // textBox2
+            // tbx_pay
             // 
-            this.textBox2.Location = new System.Drawing.Point(281, 186);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 6;
-            this.textBox2.Text = "18";
+            this.tbx_pay.Location = new System.Drawing.Point(281, 186);
+            this.tbx_pay.Name = "tbx_pay";
+            this.tbx_pay.Size = new System.Drawing.Size(100, 22);
+            this.tbx_pay.TabIndex = 6;
+            this.tbx_pay.Text = "18";
             // 
             // button1
             // 
@@ -120,13 +120,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(499, 336);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.tbx_pay);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbx_cost);
             this.Name = "Form1";
             this.Text = "405290254";
             this.ResumeLayout(false);
@@ -136,13 +136,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbx_cost;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbx_pay;
         private System.Windows.Forms.Button button1;
     }
 }
--- Hw4StoreManagement/Hw4StoreManagement/Form1.cs
+++ Hw4StoreManagement/Hw4StoreManagement/Form1.cs
@@ -16,23 +16,115 @@ namespace Hw4StoreManagement
         {
             InitializeComponent();
         }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
+        string[] food_inf = new string[] {
+            "--請輸入品名--","黑松汽水","純粹喝咖啡","純喫茶紅茶","飲冰室綠奶茶","台灣啤酒"
+        };
+        string food = "";
+        int[] cost_inf = new int[] {0,12,20,10,15,34};
+        int[] pay_inf = new int[] {0,30,30,25,25,60};
+        int cost = 0;
+        int pay = 0;
 
         private void button1_Click(object sender, EventArgs e)
         {
-            try
+            Boolean flag = false;
+            if (comboBox1.Text == "")
             {
-                
+                MessageBox.Show("品名未輸入(字串)", "警告!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
             }
-            catch
+            if (tbx_cost.Text == "")
             {
-                MessageBox.Show("請完整輸入品名、成本和售價", "");
-
+                MessageBox.Show("成本未輸入(正整數)", "警告!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
             }
+            else
+            {
+                try
+                {
+                    cost = Convert.ToInt32(tbx_cost.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("成本輸入格式不合(正整數)", "警告!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+            if (tbx_pay.Text == "")
+            {
+                MessageBox.Show("售價未輸入(正整數)", "警告!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else
+            {
+                try
+                {
+                    pay = Convert.ToInt32(tbx_pay.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("售價輸入格式不合(正整數)", "警告!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+            if (cost > pay)
+            {
+                MessageBox.Show("輸入過程出現問題" + "\n" + "成本:" + cost + ">" + "售價:" + pay, "警告!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                for (int i = 0; i < food_inf.Length; i++)
+                {
+                    if (food_inf[i].Equals(comboBox1.Text))
+                    {
+                        try
+                        {
+                            cost_inf[i] = Convert.ToInt32(tbx_cost.Text);
+                        }
+                        catch (Exception)
+                        {
+                            cost_inf[i] = 0;
+                        }
+                        try
+                        {
+                            pay_inf[i] = Convert.ToInt32(tbx_pay.Text);
+                        }
+                        catch (Exception)
+                        {
+                            pay_inf[i] = 0;
+                        }
+                        MessageBox.Show("資料修改成功!" + "\n" + "品名:" + food + "\n" + "成本:" + cost + "\n" + "售價:" + pay, "訊息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        flag = true;
+                        break;
+                    };
+                }
+                if (flag == false)
+                {
+                    System.Array.Resize(ref food_inf, food_inf.Length + 1);
+                    System.Array.Resize(ref cost_inf, cost_inf.Length + 1);
+                    System.Array.Resize(ref pay_inf, pay_inf.Length + 1);
+                    food_inf[food_inf.Length - 1] = comboBox1.Text;
+                    food = comboBox1.Text;
+                    cost_inf[cost_inf.Length - 1] = Convert.ToInt32(tbx_cost.Text);
+                    pay_inf[pay_inf.Length - 1] = Convert.ToInt32(tbx_pay.Text);
+                    MessageBox.Show("新增資料成功!" + "\n" + "品名:" + food + "\n" + "成本:" + cost + "\n" + "售價:" + pay, "訊息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                };
+            };
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(food_inf);
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            food = food_inf[comboBox1.SelectedIndex];
+            cost = cost_inf[comboBox1.SelectedIndex];
+            pay = pay_inf[comboBox1.SelectedIndex];
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(food_inf);
+            comboBox1.SelectedIndex = 0;
+        }
+
     }
 }
