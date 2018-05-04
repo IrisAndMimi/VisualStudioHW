59366c8bb3658900b06c212f21dcef42d523999b
IrisAndMimi <my1ris227@outlook.com>
2018-05-04 11:45:06 +0800

    Hw5MonthCalender

---
+++ Hw5MonthCalender/Hw5MonthCalender/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Hw5MonthCalender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(288, 69);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Hw5MonthCalender.Properties.Resources.春;
+            this.pictureBox1.Location = new System.Drawing.Point(50, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(198, 198);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 30F);
+            this.label1.Location = new System.Drawing.Point(349, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 40);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "月曆";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 293);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "405290254";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Hw5MonthCalender/Hw5MonthCalender/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hw5MonthCalender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            switch(monthCalendar1.SelectionStart.Month)
+            {
+                case 1:
+                    pictureBox1.Image = Image.FromFile("冬.jpg");
+                    break;
+                case 2:
+                    pictureBox1.Image = Image.FromFile("冬.jpg");
+                    break;
+                case 3:
+                    pictureBox1.Image = Image.FromFile("春.jpg");
+                    break;
+                case 4:
+                    pictureBox1.Image = Image.FromFile("春.jpg");
+                    break;
+                case 5:
+                    pictureBox1.Image = Image.FromFile("春.jpg");
+                    break;
+                case 6:
+                    pictureBox1.Image = Image.FromFile("夏.jpg");
+                    break;
+                case 7:
+                    pictureBox1.Image = Image.FromFile("夏.jpg");
+                    break;
+                case 8:
+                    pictureBox1.Image = Image.FromFile("夏.jpg");
+                    break;
+                case 9:
+                    pictureBox1.Image = Image.FromFile("秋.jpg");
+                    break;
+                case 10:
+                    pictureBox1.Image = Image.FromFile("秋.jpg");
+                    break;
+                case 11:
+                    pictureBox1.Image = Image.FromFile("秋.jpg");
+                    break;
+                case 12:
+                    pictureBox1.Image = Image.FromFile("冬.jpg");
+                    break;
+
+            }
+            
+       
+        }
+    }
+}
